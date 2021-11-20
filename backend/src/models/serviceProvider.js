151122b5342a500a732ac82dd8cc09311ccf93d1
samarthya-jha <samarthya3011@gmail.com>
2021-11-21 04:28:39 +0530

    Schema changes

--- backend/src/models/serviceProvider.js
+++ backend/src/models/serviceProvider.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')})
 
 const serviceProviderSchema = new mongoose.Schema({
   name: {
@@ -65,8 +68,48 @@ const serviceProviderSchema = new mongoose.Schema({
   hourlyFees:{
     type: Number,
     required: true,
-  }
+  },
+  token: {
+    type: String,
+    required: true
+  },
+  blocked: {
+    type: Boolean,
+    required: true
+  },
 });
+
+
+serviceProviderSchema.methods.generateToken = async function () {
+  const serviceProvider = this;
+  const payload = {
+    _id: serviceProvider._id
+  };
+  const token =  jwt.sign(payload, process.env.CompanySecret);
+  serviceProvider.token = token;
+  await serviceProvider.save();
+  return token;
+}
+
+serviceProviderSchema.statics.findByCredentials = (userPhone, password) => {
+  const user = await ServiceProvider.findOne({phoneNumber: userId, password: password, blocked: false, phoneNumberVerified: true});
+  if (!user) {
+    throw new Error('Unable to login');
+  } 
+  const isMatch = await bcrypt.compare(user.password, password);
+  if(!isMatch) {
+    throw new Error('Phone Number/Password is incorrect')
+  }
+  return user;
+}
+
+serviceProviderSchema.pre('save',async function (next) {
+  if(this.isModified('password')) {
+    const hash = await bcrypt.hash(this.Password, 8);
+    this.Password = hash;
+  }
+  next();
+})
 
 const ServiceProvider = mongoose.model(
   "serviceProvider",
